5cd6b30ac3bd83aaf27f338043af0df484712b9e
shanghua.zhao <shanghua@youcaideMacBook-Pro.local>
2020-05-14 11:30:24 +0800

    no message

--- demo.ts
+++ demo.ts
@@ -1,26 +1,31 @@
-// 函数返回数字类型
-function addNumber(firstNumber: number, secondNumber: number): number {
-	return firstNumber + secondNumber;
-}
+// 基础类型：number, string, boolean, void, undefined, symbol, null
+const count = 123;
+let total: number;
+total = 456;
 
-// 函数没有返回值
-function sayHello(): void {
-	console.log('hello');
-}
+// 对象类型：class, function, {}, []
+const func = (str: string): number => {
+	return parseInt(str, 10);
+};
 
-// 函数永远不会执行到最后
-function err(): never {
-	while (true) {}
-	console.log('这里不会被执行');
-}
+// 1，定义func2函数，返回传入string类型参数，并返回number类型数据
+const func2: (str: string) => number;
+// 2，定义func2函数的值，赋值一个函数体
+// 协助理解：函数冒号后边跟的是类型，等于后边跟的是函数体
+const func2: (str: string) => number = (str) => {
+	return parseInt(str, 10);
+};
+// 小技巧：函数的返回值一般可以通过类型推断，推出来，一般不需要写类型
 
-// 函数解构参数类型
-function add({ first, second }: { first: number; second: number }): number {
-	return first + second;
+// 其他知识点
+const data = '{name:xiaoming, arg:18}';
+// newData是any类型，需要类型注解
+let newData = JSON.parse(data);
+interface Persion {
+	name: 'string';
+	age: 'number';
 }
-const count = add({ first: 1, second: 2 });
+let newData: Persion = JSON.parse(data);
 
-function getNumber({ first }: { first: number }) {
-	return first;
-}
-const total = getNumber({ first: 3 });
+let tmp: number | string = 123;
+tmp = '456';
