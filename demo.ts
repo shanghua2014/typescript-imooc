b92d5f70d5cc7deedb0d799d0a9e34f446175c36
shanghua.zhao <shanghua@youcaideMacBook-Pro.local>
2020-05-14 10:38:31 +0800

    no message

--- demo.ts
+++ demo.ts
@@ -1,31 +1,19 @@
-interface Point {
-    x: number,
-    y: number
+// type annotation 类型注解，我们主动告诉 TS 变量是是什么类型
+// type inference 类型推断，TS 自动尝试去分解变量类型
+// 如果 TS 能主动分析变量类型，我们什么都不需要做
+// 如果 TS 无法分析亦是类型，我们就需要使用类型注解
+
+// let count: number;
+// count = 123;
+// let countInference = 456;
+
+// 类型推断
+const firstNumber = 1;
+const secondNumber = 2;
+const total = firstNumber + secondNumber;
+
+// 类型注解
+function getTotal(firstNumber: number, secondNumber: number) {
+	return firstNumber + secondNumber;
 }
-
-const count: Point = {
-    x:3,
-    y:4
-}
-
-const abc: Point = {
-    x:5,
-    y:6
-}
-
-const teacher : {
-    name:string,
-    age:number
-} = {
-    name: "abc",
-    age :13
-}
-
-class Person {}
-const xiaoming : Person = new Person();
-
-const numberArr: number[] = [1,2,3];
-
-const getTotal: () => number = () => {
-    return 1;
-}
+getTotal(3, 4);
