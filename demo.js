c2011f829b3bea200e5af90d47719ce10aa36880
shanghua.zhao <shanghua@youcaideMacBook-Pro.local>
2020-05-14 10:55:46 +0800

    no message

--- demo.js
+++
@@ -1,6 +0,0 @@
-function abc(data) {
-    console.log("abs");
-    console.log(data.x);
-    return data.x + data.y;
-}
-abc({ x: 1, y: 2 });
--- demo.ts
+++ demo.ts
@@ -1,19 +1,26 @@
-// type annotation 类型注解，我们主动告诉 TS 变量是是什么类型
-// type inference 类型推断，TS 自动尝试去分解变量类型
-// 如果 TS 能主动分析变量类型，我们什么都不需要做
-// 如果 TS 无法分析亦是类型，我们就需要使用类型注解
-
-// let count: number;
-// count = 123;
-// let countInference = 456;
-
-// 类型推断
-const firstNumber = 1;
-const secondNumber = 2;
-const total = firstNumber + secondNumber;
-
-// 类型注解
-function getTotal(firstNumber: number, secondNumber: number) {
+// 函数返回数字类型
+function addNumber(firstNumber: number, secondNumber: number): number {
 	return firstNumber + secondNumber;
 }
-getTotal(3, 4);
+
+// 函数没有返回值
+function sayHello(): void {
+	console.log('hello');
+}
+
+// 函数永远不会执行到最后
+function err(): never {
+	while (true) {}
+	console.log('这里不会被执行');
+}
+
+// 函数解构参数类型
+function add({ first, second }: { first: number; second: number }): number {
+	return first + second;
+}
+const count = add({ first: 1, second: 2 });
+
+function getNumber({ first }: { first: number }) {
+	return first;
+}
+const total = getNumber({ first: 3 });
