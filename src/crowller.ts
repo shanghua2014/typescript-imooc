21d798f5a40ee04223bc35f1e564063d8b6d5010
shanghua.zhao <shanghua@youcaideMacBook-Pro.local>
2020-05-15 14:51:42 +0800

    no message

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,10 +1,10 @@
 // ts直接引入js，无法识别js文件，需要 .d.ts 的翻译文件
 import superagent from 'superagent';
+import cheerio from 'cheerio';
 
 class Crowller {
 	private secret = 'x3b174jsx';
 	private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
-	private rawHtml = '';
 
 	constructor() {
 		this.getRawHtml();
@@ -12,8 +12,32 @@ class Crowller {
 
 	async getRawHtml() {
 		const result = await superagent.get(this.url);
-		this.rawHtml = result.text;
-		console.log(this.rawHtml);
+		this.getCourseInfo(result.text);
+	}
+
+	getCourseInfo(html: string) {
+		interface Course {
+			title: string;
+			count: number;
+		}
+
+		const $ = cheerio.load(html);
+		const courseInfos: Course[] = [];
+
+		$('.course-item').map((index, element) => {
+			const descs = $(element).find('.course-desc');
+			const title = descs.eq(0).text();
+			const count = parseInt(descs.eq(1).text().split('：')[1]);
+			courseInfos.push({
+				title,
+				count
+			});
+		});
+		const result = {
+			time: new Date().getTime(),
+			data: courseInfos
+		};
+		console.log(result);
 	}
 }
 
