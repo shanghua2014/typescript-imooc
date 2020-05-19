769a2490f0fc2f82f58a457fac3bad0ada96c45e
shanghua.zhao <shanghua@youcaideMacBook-Pro.local>
2020-05-19 11:48:22 +0800

    no message

---
+++ build/dellAnalyzer.js
@@ -0,0 +1,51 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = __importDefault(require("fs"));
+var cheerio_1 = __importDefault(require("cheerio"));
+// 继承 Analyzer是为了规定类里必有一个 analyze 方法
+var DellAnalyzer = /** @class */ (function () {
+    function DellAnalyzer() {
+    }
+    // 单例
+    DellAnalyzer.getInstance = function () {
+        if (!DellAnalyzer.instance) {
+            DellAnalyzer.instance = new DellAnalyzer();
+        }
+        return DellAnalyzer.instance;
+    };
+    DellAnalyzer.prototype.getCourseInfo = function (html) {
+        var $ = cheerio_1.default.load(html);
+        var courseInfos = [];
+        $('.course-item').map(function (index, element) {
+            var descs = $(element).find('.course-desc');
+            var title = descs.eq(0).text();
+            var count = parseInt(descs.eq(1).text().split('：')[1]);
+            courseInfos.push({
+                title: title,
+                count: count
+            });
+        });
+        return {
+            time: new Date().getTime(),
+            data: courseInfos
+        };
+    };
+    DellAnalyzer.prototype.generateJsonContent = function (courseInfo, filePath) {
+        var fileContent = {};
+        if (fs_1.default.existsSync(filePath)) {
+            fileContent = JSON.parse(fs_1.default.readFileSync(filePath, 'utf-8'));
+        }
+        fileContent[courseInfo.time] = courseInfo.data;
+        return fileContent;
+    };
+    DellAnalyzer.prototype.analyze = function (html, filePath) {
+        var courseInfo = this.getCourseInfo(html);
+        var fileContent = this.generateJsonContent(courseInfo, filePath);
+        return JSON.stringify(fileContent);
+    };
+    return DellAnalyzer;
+}());
+exports.default = DellAnalyzer;
